32f04ac24418dd68562d3da507581b71212cf70d
abubakar shiddiq <abenk223jr@gmail.com>
2026-04-08 22:16:20 +0800

    baru

--- src/pages/BirthdayCountdown.jsx
+++ src/pages/BirthdayCountdown.jsx
@@ -62,7 +62,7 @@ const BirthdayCountdown = ({ isIOS }) => {
         className="text-center max-w-2xl"
       >
         <h1 className="text-4xl md:text-5xl font-bold text-pink-600 mb-8">
-          The Birthday of <span className="text-purple-600">Aprillia Krisnawati</span>
+          The Birthday of <span className="text-purple-600">Zainab</span>
         </h1>
 
         <div className="mb-8">
@@ -108,7 +108,7 @@ const BirthdayCountdown = ({ isIOS }) => {
 
         <div className="bg-white p-6 rounded-xl shadow-lg mb-8 border border-pink-100">
           <h3 className="text-xl font-semibold text-purple-600 mb-2">
-            📅 7 April 2026
+            📅 9 april 2026
           </h3>
           <p className="text-pink-600">
            Tanggal dimana senyumanmu akan menerangi hariku
